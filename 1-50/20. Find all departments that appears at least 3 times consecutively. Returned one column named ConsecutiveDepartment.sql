052421b213ddd35f7c6fba31d08169a906b3d516
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2026-01-19 18:11:08 +0530

    Update 20. Find all departments that appears at least 3 times consecutively. Returned one column named ConsecutiveDepartment.sql

--- 1-50/20. Find all departments that appears at least 3 times consecutively. Returned one column named ConsecutiveDepartment.sql
+++ 1-50/20. Find all departments that appears at least 3 times consecutively. Returned one column named ConsecutiveDepartment.sql
@@ -5,3 +5,24 @@ join employees e2
 on e1.employee_id+1=e2.employee_id and e1.department=e2.department
 join employees e3
 on e2.employee_id+2=e3.employee_id and e2.department=e3.department;
+
+
+--? I found this approach on someone's linkedin post  
+with cte as (
+select department,LEAD(department,1) over(ORDER BY employee_id) as next_dpt,
+LEAD(department,2) over(ORDER BY employee_id) as next_next_dpt
+from employees
+)
+select DISTINCT department as ConsecutiveDepartment
+from cte
+where department=next_dpt and next_dpt=next_next_dpt
+
+-- Above ones are mostly preferable
+-- Using Subquery
+select DISTINCT department
+from (
+select department,LEAD(department,1) over(ORDER BY employee_id) as next_dpt,
+LEAD(department,2) over(ORDER BY employee_id) as next_next_dpt
+from employees
+) as subquery
+WHERE department=next_dpt and next_dpt=next_next_dpt
