03ed1f435ec2a0d46485a182aa1e87c5bbee88e2
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2026-02-14 09:49:48 +0530

    Add INTERSECT approach for customer purchase query
    Added a second approach using INTERSECT to find customers who made purchases in each year from 2023 to 2025.

--- 1-50/26. Find customers who have made a purchase in every single year from 2023 to 2025.sql
+++ 1-50/26. Find customers who have made a purchase in every single year from 2023 to 2025.sql
@@ -6,3 +6,17 @@ USING(customer_id)
 where YEAR(order_date) BETWEEN 2023 and 2025
 GROUP BY 1
 HAVING COUNT(DISTINCT YEAR(order_date)) = 3;
+
+
+--? Second Approach Suggested by a user on Linkedin  (using INTERSECT)
+select customer_id
+from orders
+where YEAR(order_date)=2023
+INTERSECT
+select customer_id
+from orders
+where YEAR(order_date)=2024
+INTERSECT
+select customer_id
+from orders
+where YEAR(order_date)=2025
